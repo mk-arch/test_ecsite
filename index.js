94155e9141724f6a102833b247ffc44a045cb493
minoru <kmuibnootrauout@gmail.com>
2022-04-09 10:13:27 +0900

    first

---
+++ index.js
@@ -0,0 +1,60 @@
+// 業界
+const industries = ['manufacture', 'financial', 'real_estate']
+
+// 売上と利益のデータ
+const report = [
+  {
+    industry: 'manufacture',
+    department: 'chemical',
+    domestic: 8060786,
+    overseas: 7047191,
+  },
+  {
+    industry: 'financial',
+    department: 'insurance',
+    domestic: 9859531,
+    overseas: 7620080,
+  },
+  {
+    industry: 'manufacture',
+    department: 'paper',
+    domestic: 3373828,
+    overseas: 5384591,
+  },
+  {
+    industry: 'real_estate',
+    department: 'house',
+    domestic: 8094797,
+    overseas: 5303474,
+  },
+  {
+    industry: 'financial',
+    department: 'securities',
+    domestic: 8045496,
+    overseas: 9235759,
+  },
+  {
+    industry: 'real_estate',
+    department: 'hotel',
+    domestic: 3806430,
+    overseas: 5303474,
+  },
+  {
+    industry: 'manufacture',
+    department: 'oil',
+    domestic: 5453934,
+    overseas: 1977028,
+  },
+  {
+    industry: 'real_estate',
+    department: 'apartment',
+    domestic: 7803898,
+    overseas: 6480764,
+  },
+  {
+    industry: 'manufacture',
+    department: 'steel',
+    domestic: 9085945,
+    overseas: 5485068,
+  },
+]
